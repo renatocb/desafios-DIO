1c93508eb4e5fcf47af72fd09e3740c12fdbccd2
Renato Correa Bento <renato.bento@talentfour.com.br>
2023-08-14 10:22:48 -0300

    Criando estrutura de pastas

---
+++ 5. Desafios/OrientacaoObjeto/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Nokia");
+Nokia nokia = new Nokia("1500", "Mokia N95", "1244444", 64);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whats");
+
+Console.WriteLine("Iphone");
+Iphone iphone = new Iphone("3500", "Iphone XII", "250086", 256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
